977639ecc614a2ee7d388c329fa5392927662e06
totti29 <frankyvega2905@gmail.com>
2023-04-15 18:36:46 -0500

    Se cambiaron los verbos de los controladores a español.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,7 +23,12 @@ class RouteServiceProvider extends ServiceProvider
      * Define your route model bindings, pattern filters, and other route configuration.
      */
     public function boot(): void
-    {
+    { 
+        Route::resourceVerbs([
+            'create' => 'crear',
+            'edit' => 'editar',
+        ]);
+        
         $this->configureRateLimiting();
 
         $this->routes(function () {
