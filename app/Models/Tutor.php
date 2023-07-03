5fe7379ab9e1f97f84758485c52204987095850a
Héctor Villazón <hdvillazon@hotmail.com>
2023-07-03 18:10:17 -0500

    Se actualizó el modelo tutor con su relación hacia grupo

--- app/Models/Tutor.php
+++ app/Models/Tutor.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tutor extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-    protected $table = "tutores";
+	protected $table = "tutores";
+
+	protected $with = "grupo";
+
+	public function grupo(): BelongsTo
+	{
+		return $this->belongsTo(Grupo::class);
+	}
 }
